6d0d0bdb3e533b7c239a43046b10b42ce47e4a17
Denis Rykovanov <rykovanov.denis@yandex.ru>
2025-01-11 19:32:14 +0300

    скрипты старта двух шардов

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -1,11 +1,85 @@
 #!/bin/bash
 
+set -x
+set -e
+
+###
+# Инициализируем конфиг сервер
+###
+
+docker-compose exec -T configSrv mongosh --port 27017 <<EOF
+
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+exit();
+EOF
+
+
+###
+# Инициализируем шарды
+###
+
+docker-compose exec -T shard1 mongosh --port 27018 <<EOF
+
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+       // { _id : 1, host : "shard2:27019" }
+      ]
+    }
+);
+exit();
+EOF
+
+docker-compose exec -T shard2 mongosh --port 27019 <<EOF
+
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+       // { _id : 0, host : "shard1:27018" },
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+exit();
+EOF
+
+###
+# Инициализируем роутер
+###
+
+docker-compose exec -T mongos_router mongosh --port 27020 <<EOF
+
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+
+sh.enableSharding("mongodb1");
+sh.shardCollection("mongodb1.helloDoc", { "name" : "hashed" } );
+exit();
+EOF
+
+
 ###
 # Инициализируем бд
 ###
 
-docker compose exec -T mongodb1 mongosh <<EOF
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+
+use mongodb1
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments() 
+exit();
 EOF
 
